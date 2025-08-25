2894051bc9ffda2ec0003d9dd4359a87de80c005
MacBookPro <lezie04@gmail.com>
2025-08-25 16:37:43 +0000

    modfiocation du http://109.199.107.23:4000/api

--- types/baseUrl.ts
+++ types/baseUrl.ts
@@ -9,7 +9,9 @@
 // baseUrl.ts
 
 // On récupère la variable d'environnement ou on met une valeur par défaut
-const BASE_URL = process.env.NEXT_PUBLIC_SERVEUR_URL || 'http://localhost:4000/api/v1';
+// const BASE_URL = process.env.NEXT_PUBLIC_SERVEUR_URL || 'http://localhost:4000/api/v1';
+const BASE_URL = process.env.NEXT_PUBLIC_SERVEUR_URL || 'http://109.199.107.23:4000/api/v1';
+
 export const getBaseUrl = (): string => {
     return BASE_URL;
 };
